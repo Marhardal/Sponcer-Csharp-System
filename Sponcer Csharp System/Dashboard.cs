12dd18b3a5e5dbec4e2408003d2aa79cbc584b58
Martin <martindharawa@gmail.com>
2022-12-02 07:48:39 +0200

    Managed to write the backend code for the sponcer and school where user can finaly insert, update, delete and view all the intered sponcers.

--- Sponcer Csharp System/Dashboard.cs
+++ Sponcer Csharp System/Dashboard.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,6 +19,9 @@ namespace Sponcer_Csharp_System
             InitializeComponent();
         }
 
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ToString());
+        SqlCommand com = new SqlCommand();
+
         private void Homebtn_Click(object sender, EventArgs e)
         {
             Studentdgv.SetPage("Home");
@@ -25,12 +30,67 @@ namespace Sponcer_Csharp_System
             Image.Image = img;
         }
 
+        void sponcer()
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Closed) 
+                {
+                    connection.Open();
+                    string sql = "Select * from Sponcer";
+                    com = new SqlCommand(sql, connection);
+                    SqlDataAdapter sqlData = new SqlDataAdapter(com);
+                    DataTable dt = new DataTable();
+                    sqlData.Fill(dt);
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        Sponcerdgv.DataSource = dt;
+                    }
+                    sqlData.Dispose();
+                    dt.Dispose();
+                    connection.Close();
+                }
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+        }
+
         private void Sponcerbtn_Click(object sender, EventArgs e)
         {
             Studentdgv.SetPage("Sponcer");
             headinglbl.Text = "Sponcer System\\Sponcer";
             Image img = System.Drawing.Image.FromFile(Application.StartupPath + @"\Icons\parenting_50px.png");
             Image.Image = img;
+            sponcer();
+        }
+
+        void school()
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Closed) 
+                {
+                    connection.Open();
+                    string sql = "Select * from school";
+                    com = new SqlCommand(sql, connection);
+                    SqlDataAdapter sda = new SqlDataAdapter(com);
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        Schooldgv.DataSource = dt;
+                    }
+                    sda.Dispose();
+                    dt.Dispose();
+                    connection.Close();
+                }
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
         }
 
         private void Schoolbtn_Click(object sender, EventArgs e)
@@ -39,6 +99,7 @@ namespace Sponcer_Csharp_System
             headinglbl.Text = "Sponcer System\\Schools";
             Image img = System.Drawing.Image.FromFile(Application.StartupPath + @"\Icons\school_house_60px.png");
             Image.Image = img;
+            school();
         }
 
         private void Classbtn_Click(object sender, EventArgs e)
@@ -67,7 +128,7 @@ namespace Sponcer_Csharp_System
 
         private void gunaButton1_Click(object sender, EventArgs e)
         {
-
+            Sponcerdgv.Refresh();
         }
 
         private void Sponcerpribtn_Click(object sender, EventArgs e)
@@ -86,8 +147,7 @@ namespace Sponcer_Csharp_System
             Sponcer sponcer = new Sponcer();
             sponcer.Location = new Point(0, 170);
             sponcer.Insertbtn.BringToFront();
-            sponcer.bunifuCustomLabel1.Text = "Enter Sponser";
-            sponcer.bunifuCustomLabel2.Text = "Enter Spocer Name";
+            sponcer.bunifuCustomLabel2.Text = "Create Sponcer";
             upsert.Controls.Add(sponcer);
             upsert.Show();
         }
@@ -102,6 +162,94 @@ namespace Sponcer_Csharp_System
             sponcer.bunifuCustomLabel2.Text = "Update Spocer Name";
             upsert.Controls.Add(sponcer);
             upsert.Show();
+        }
+
+        private void Sponcerdelbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var id = Sponcerdgv.CurrentRow.Cells[0].Value.ToString();
+                if (connection.State == ConnectionState.Closed) 
+                {
+                    connection.Open();
+                    string sql = "Delete from Sponcer Where ID='" + id + "'";
+                    com = new SqlCommand(sql, connection);
+                    var res = com.ExecuteNonQuery();
+                    if (res > 0)
+                    {
+                        MessageBox.Show("Sponcer Deleted!! Please refresh the grid.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sponcer Not Deleted. Please try again later.");
+                    }
+                }
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+        }
+
+        private void Schoolrefreshbtn_Click(object sender, EventArgs e)
+        {
+            school();
+            Schooldgv.Refresh();
+        }
+
+        private void Schoolinsbtn_Click(object sender, EventArgs e)
+        {
+            Upsert upsert = new Upsert();
+            School school = new School();
+            school.Location = new Point(Left, Top);
+            school.bunifuCustomLabel2.Text = "Create School";
+            upsert.Controls.Add(school);
+            upsert.Show();
+        }
+
+        private void Schoolupdbtn_Click(object sender, EventArgs e)
+        {
+            Upsert upsert = new Upsert();
+            School school = new School();
+            school.Location = new Point(Left, Top);
+            school.bunifuCustomLabel2.Text = "Update School";
+            school.nmtxt.Text = Schooldgv.CurrentRow.Cells[1].Value.ToString();
+            school.loctxt.Text = Schooldgv.CurrentRow.Cells[2].Value.ToString();
+            school.faccmd.SelectedItem(Schooldgv.CurrentRow.Cells[3].Value.ToString());
+            school.posttxt.Text = Schooldgv.CurrentRow.Cells[4].Value.ToString();
+            school.typcmd.SelectedItem(Schooldgv.CurrentRow.Cells[5].Value.ToString());
+            school.feestxt.Text = Schooldgv.CurrentRow.Cells[6].Value.ToString();
+            school.id = Schooldgv.CurrentRow.Cells[0].Value.ToString();
+            upsert.Controls.Add(school);
+            upsert.Show();
+        }
+
+        private void Schooldelbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                    string delete = "Delete from School where ID=@id";
+                    com = new SqlCommand(delete, connection);
+                    com.Parameters.Add(new SqlParameter("@id", Schooldgv.CurrentRow.Cells[0].Value.ToString()));
+                    var res = com.ExecuteNonQuery();
+                    if (res > 0)
+                    {
+                        MessageBox.Show("School Deleted Successfully.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to delete school, Please try again later.");
+                    }
+                    connection.Close();
+                }
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
         }
     }
 }
--- Sponcer Csharp System/School.cs
+++ Sponcer Csharp System/School.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,105 @@ namespace Sponcer_Csharp_System
         public School()
         {
             InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ToString());
+        SqlCommand com = new SqlCommand();
+
+        private void Insertbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (nmtxt.Text != "" || loctxt.Text != "" || posttxt.Text != "" || faccmd.selectedIndex != 0 || typcmd.selectedIndex != 0 || feestxt.Text != "") 
+                {
+                    if (connection.State == ConnectionState.Closed)
+                    {
+                        connection.Open();
+                        string sql = "Select * from School where Location=@loc and Faculty=@fac and [Postal Address]=@pos and Type=@typ";
+                        com=new SqlCommand(sql, connection);
+                        com.Parameters.Add(new SqlParameter("@fac", faccmd.selectedValue));
+                        com.Parameters.Add(new SqlParameter("@pos", posttxt.Text));
+                        com.Parameters.Add(new SqlParameter("@loc", loctxt.Text));
+                        com.Parameters.Add(new SqlParameter("@typ", typcmd.selectedValue));
+                        SqlDataReader sqlData= com.ExecuteReader();
+                        if (sqlData.HasRows)
+                        {
+                            sqlData.Read();
+                            MessageBox.Show("School already exist, Please change the details to continue.");
+                            sqlData.Close();
+                        }
+                        else
+                        {
+                            string insert = "insert into School values(@nm, @loc, @fac, @pos, @typ, @fee, now())";
+                            com =new SqlCommand(insert, connection);
+                            com.Parameters.Add(new SqlParameter("@fac", faccmd.selectedValue));
+                            com.Parameters.Add(new SqlParameter("@pos", posttxt.Text));
+                            com.Parameters.Add(new SqlParameter("@loc", loctxt.Text));
+                            com.Parameters.Add(new SqlParameter("@typ", typcmd.selectedValue));
+                            com.Parameters.Add(new SqlParameter("nm", nmtxt.Text));
+                            com.Parameters.Add(new SqlParameter("@fee", feestxt));
+                            var res=com.ExecuteNonQuery();
+                            if (res != 0)
+                            {
+                                Hide();
+                                Parent.Hide();
+                                MessageBox.Show("School created successfully!! Please Refresh the grid");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Failed to create school.");
+                            }
+                        }
+                        connection.Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Fill in all the fields to continue");
+                }
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+        }
+
+        internal string id;
+
+        private void Updatebtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Closed) 
+                {
+                    connection.Open();
+                    string update = "update School set Name=@nm, Location=@loc, Faculty=@fac, [Postal Address]=@pos, Type=@typ, Fees=@fee Where ID=@id;";
+                    com=new SqlCommand(update, connection);
+                    com.Parameters.Add(new SqlParameter("@fac", faccmd.selectedValue));
+                    com.Parameters.Add(new SqlParameter("@pos", posttxt.Text));
+                    com.Parameters.Add(new SqlParameter("@loc", loctxt.Text));
+                    com.Parameters.Add(new SqlParameter("@typ", typcmd.selectedValue));
+                    com.Parameters.Add(new SqlParameter("nm", nmtxt.Text));
+                    com.Parameters.Add(new SqlParameter("@fee", feestxt));
+                    com.Parameters.Add(new SqlParameter("id", id));
+                    var res = com.ExecuteNonQuery();
+                    if (res > 0)
+                    {
+                        Hide();
+                        Parent.Hide();
+                        MessageBox.Show("School Updated Successfully!! Please Refresh the grid to see the changes");
+                    }
+                    else
+                    {
+                        MessageBox.Show("School Not Updated, Please try again later.");
+                    }
+                    connection.Close();
+                }
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
         }
     }
 }
